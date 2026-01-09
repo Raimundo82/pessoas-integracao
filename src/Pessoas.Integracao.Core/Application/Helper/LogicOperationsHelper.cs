8524371be359a878c731d510b94212d43e8aa390
1TEN EN-AEL Santos Carapau <santos.carapau@marinha.pt>
2026-01-09 14:47:54 +0000

    feat: added feat to merge data from source and repository

---
+++ src/Pessoas.Integracao.Core/Application/Helper/LogicOperationsHelper.cs
@@ -0,0 +1,22 @@
+namespace Pessoas.Integracao.Core.Application.Helper;
+
+public static class LogicOperationsHelper
+{
+    public static IReadOnlyCollection<T> UnionBy<T, TKey>(
+    IReadOnlyCollection<T> first,
+    IReadOnlyCollection<T>? second,
+    Func<T, TKey> keySelector)
+    {
+        first ??= Array.Empty<T>();
+        second ??= Array.Empty<T>();
+
+        if (first.Count == 0) return second;
+        if (second.Count == 0) return first;
+
+        return first
+        .Concat(second)
+        .GroupBy(keySelector)
+        .Select(g => g.First())
+        .ToList();
+    }
+}
--- src/Pessoas.Integracao.Core/Application/UseCases/ImportAllPessoas.cs
+++ src/Pessoas.Integracao.Core/Application/UseCases/ImportAllPessoas.cs
@@ -1,5 +1,6 @@
 using Pessoas.Integracao.Core.Application.Abstractions;
 using Pessoas.Integracao.Core.Application.Contracts;
+using Pessoas.Integracao.Core.Application.Helper;
 using Pessoas.Integracao.Core.Domain.Interfaces;
 
 namespace Pessoas.Integracao.Core.Application.UseCases;
@@ -11,7 +12,10 @@ public sealed class ImportAllPessoas(IPessoaRepository pessoaRepository, IPessoa
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     public async Task ExecuteAsync(CancellationToken ct)
     {
-        var pessoas = await _pessoasProvider.GetPessoasAsync(ct);
+        var pessoasInSource = await _pessoasProvider.GetPessoasAsync(ct);
+        var pessoasInDb = await _pessoaRepository.GetAllAsync(ct);
+        var pessoas = LogicOperationsHelper.UnionBy(pessoasInSource, pessoasInDb, p => p.ExternalId);
+
         await _pessoaRepository.AddOrUpdateAllAsync(pessoas, ct);
         await _unitOfWork.CommitAsync(ct);
     }
