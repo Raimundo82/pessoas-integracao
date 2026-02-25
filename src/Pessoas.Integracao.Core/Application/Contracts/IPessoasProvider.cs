6381a2110abde47c95fb8aff2987fa68f2b1973b
Raimundo <pacheco.raimundo@marinha.pt>
2026-02-25 19:03:01 +0000

    refactor: rename import method in IPessoasProvider for clarity

--- src/Pessoas.Integracao.Core/Application/Contracts/IPessoasProvider.cs
+++ src/Pessoas.Integracao.Core/Application/Contracts/IPessoasProvider.cs
@@ -5,7 +5,7 @@ namespace Pessoas.Integracao.Core.Application.Contracts;
 
 public interface IPessoasProvider
 {
-    Task<IReadOnlyList<Pessoa>> GetPessoasAsync(IReadOnlyList<PessoaImportKey> pessoaImportKeys, CancellationToken cancellationToken);
+    Task<IReadOnlyList<Pessoa>> GetPessoasByImportKeysAsync(IReadOnlyList<PessoaImportKey> pessoaImportKeys, CancellationToken cancellationToken);
     Task<IReadOnlyList<PessoaImportKey>> GetSourceImportKeysAsync(CancellationToken cancellationToken);
 
 }
