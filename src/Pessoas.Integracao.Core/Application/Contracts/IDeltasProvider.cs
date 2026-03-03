e5718195d27f056ee6bc8d7fa6bc99d597c63db7
1TEN EN-AEl Santos Carapau <santos.carapau@marinha.pt>
2026-03-03 15:26:31 +0000

    refactor: removed provider level code

--- src/Pessoas.Integracao.Core/Application/Contracts/IDeltasProvider.cs
+++
@@ -1,8 +0,0 @@
-using Pessoas.Integracao.Core.Domain.Entities;
-
-namespace Pessoas.Integracao.Core.Application.Contracts;
-
-public interface IDeltasProvider
-{
-    Task<IReadOnlyList<Delta>> GetDeltasAsync(CancellationToken cancellationToken);
-}
