c5b94597e3dc51e8fc78872dc88b0ad288914b49
Raimundo <pacheco.raimundo@marinha.pt>
2026-03-04 17:43:45 +0000

    chore: fix namespaces (#396)
    ## Checklist
    
    - [X] Branch name follows **Conventional Branch**
    - [X] PR titles and Commit messages follow **Conventional Commits**
    
    Reviewed-on: https://devops-01.marinha.pt/marinha-si/pessoas-integracao/pulls/396
    Co-authored-by: Raimundo <pacheco.raimundo@marinha.pt>
    Co-committed-by: Raimundo <pacheco.raimundo@marinha.pt>

--- tests/Pessoas.Integracao.Tests/Unit/Models/TimePeriodUnitTests.cs
+++ tests/Pessoas.Integracao.Tests/Unit/Models/TimePeriodUnitTests.cs
@@ -4,78 +4,77 @@ using FluentAssertions;
 
 using Pessoas.Integracao.Core.Application.Models;
 
-namespace Pessoas.Integracao.Worker.Tests.Unit
+namespace Pessoas.Integracao.Tests.Unit.Models;
+
+public sealed class TimePeriodTests
 {
-    public sealed class TimePeriodTests
+    [Fact]
+    public void ShouldThrow_WhenEndIsBeforeStart()
     {
-        [Fact]
-        public void ShouldThrow_WhenEndIsBeforeStart()
+        Action act = () =>
         {
-            Action act = () =>
-            {
-                var _ = new TimePeriod(
-                DateTime.Parse("2020-11-25 10:00:00", CultureInfo.InvariantCulture),
-                DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture));
-            };
+            var _ = new TimePeriod(
+            DateTime.Parse("2020-11-25 10:00:00", CultureInfo.InvariantCulture),
+            DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture));
+        };
 
-            act.Should().Throw<ArgumentException>()
-               .WithMessage("*End timestamp cannot be earlier than start timestamp*");
-        }
+        act.Should().Throw<ArgumentException>()
+           .WithMessage("*End timestamp cannot be earlier than start timestamp*");
+    }
 
-        [Fact]
-        public void ShouldAllowEqualStartAndEnd_WhenStartEqualsEnd()
-        {
-            var timestamp = DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture);
+    [Fact]
+    public void ShouldAllowEqualStartAndEnd_WhenStartEqualsEnd()
+    {
+        var timestamp = DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture);
 
-            var timePeriod = new TimePeriod(timestamp, timestamp);
+        var timePeriod = new TimePeriod(timestamp, timestamp);
 
-            timePeriod.Start.Should().Be(timestamp);
-            timePeriod.End.Should().Be(timestamp);
-        }
+        timePeriod.Start.Should().Be(timestamp);
+        timePeriod.End.Should().Be(timestamp);
+    }
 
-        [Fact]
-        public void ShouldSetStartAndEndCorrectly_WhenRangeIsValid()
-        {
-            var start = DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture);
-            var end = DateTime.Parse("2020-11-24 12:00:00", CultureInfo.InvariantCulture);
+    [Fact]
+    public void ShouldSetStartAndEndCorrectly_WhenRangeIsValid()
+    {
+        var start = DateTime.Parse("2020-11-24 10:00:00", CultureInfo.InvariantCulture);
+        var end = DateTime.Parse("2020-11-24 12:00:00", CultureInfo.InvariantCulture);
 
-            var timePeriod = new TimePeriod(start, end);
+        var timePeriod = new TimePeriod(start, end);
 
-            timePeriod.Start.Should().Be(start);
-            timePeriod.End.Should().Be(end);
-        }
+        timePeriod.Start.Should().Be(start);
+        timePeriod.End.Should().Be(end);
+    }
 
-        [Fact]
-        public void ShouldReturnExpectedFormattedString_WhenStartAsStringCalledWithValidTimestamp()
-        {
-            var start = DateTime.Parse("2020-11-24 10:05:30", CultureInfo.InvariantCulture);
-            var timePeriod = new TimePeriod(start, start);
+    [Fact]
+    public void ShouldReturnExpectedFormattedString_WhenStartAsStringCalledWithValidTimestamp()
+    {
+        var start = DateTime.Parse("2020-11-24 10:05:30", CultureInfo.InvariantCulture);
+        var timePeriod = new TimePeriod(start, start);
 
-            timePeriod.StartAsString().Should().Be("2020-11-24 10:05:30");
-        }
+        timePeriod.StartAsString().Should().Be("2020-11-24 10:05:30");
+    }
 
-        [Fact]
-        public void ShouldReturnExpectedFormattedString_WhenEndAsStringCalledWithValidTimestamp()
-        {
-            var end = DateTime.Parse("2020-11-24 18:45:10", CultureInfo.InvariantCulture);
-            var timePeriod = new TimePeriod(end, end);
+    [Fact]
+    public void ShouldReturnExpectedFormattedString_WhenEndAsStringCalledWithValidTimestamp()
+    {
+        var end = DateTime.Parse("2020-11-24 18:45:10", CultureInfo.InvariantCulture);
+        var timePeriod = new TimePeriod(end, end);
 
-            timePeriod.EndAsString().Should().Be("2020-11-24 18:45:10");
-        }
+        timePeriod.EndAsString().Should().Be("2020-11-24 18:45:10");
+    }
 
-        [Fact]
-        public void ShouldKeepFormatting_WhenAsStringMethodsAreCultureIndependent()
-        {
-            var start = new DateTime(2020, 11, 24, 10, 0, 0, DateTimeKind.Unspecified);
-            var end = new DateTime(2020, 11, 24, 11, 0, 0, DateTimeKind.Unspecified);
+    [Fact]
+    public void ShouldKeepFormatting_WhenAsStringMethodsAreCultureIndependent()
+    {
+        var start = new DateTime(2020, 11, 24, 10, 0, 0, DateTimeKind.Unspecified);
+        var end = new DateTime(2020, 11, 24, 11, 0, 0, DateTimeKind.Unspecified);
 
-            var timePeriod = new TimePeriod(start, end);
+        var timePeriod = new TimePeriod(start, end);
 
-            var formattedStart = timePeriod.StartAsString();
-            var formattedEnd = timePeriod.EndAsString();
+        var formattedStart = timePeriod.StartAsString();
+        var formattedEnd = timePeriod.EndAsString();
 
-            formattedStart.Should().Be("2020-11-24 10:00:00");
-            formattedEnd.Should().Be("2020-11-24 11:00:00");
-        }
+        formattedStart.Should().Be("2020-11-24 10:00:00");
+        formattedEnd.Should().Be("2020-11-24 11:00:00");
     }
 }
