1fbaf6029200a58a109ef195d9d35186078642be
João Raimundo <joao.raimundo.1982@gmail.com>
2026-03-04 08:53:41 +0000

    refactor: update ISoapChannelProvider and SoapChannelProvider to remove endpointUrl parameter from CreateChannel method and use DI ChannelFactory

--- src/Pessoas.Integracao.Worker/Infrastructure/Sigdn.Rh/Soap/Channel/SoapChannelProvider.cs
+++ src/Pessoas.Integracao.Worker/Infrastructure/Sigdn.Rh/Soap/Channel/SoapChannelProvider.cs
@@ -1,9 +1,9 @@
+using System.ServiceModel;
+
 namespace Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Channel;
 
-public class SoapChannelProvider<TChannel> : ISoapChannelProvider<TChannel>
+public class SoapChannelProvider<TChannel>(ChannelFactory<TChannel> channelFactory) : ISoapChannelProvider<TChannel>
 {
-    public TChannel CreateChannel(string endpointUrl)
-    {
-        return SoapChannelFactory.CreateChannelFactory<TChannel>(endpointUrl).CreateChannel();
-    }
+    private readonly ChannelFactory<TChannel> _channelFactory = channelFactory;
+    public TChannel CreateChannel() => _channelFactory.CreateChannel();
 }
