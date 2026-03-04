5fe748c83ad28a49aee504c135768a14e092caac
João Raimundo <joao.raimundo.1982@gmail.com>
2026-03-04 08:16:03 +0000

    refactor: enhance SOAP channel factory with new extension methods and service registrations to avoid multiple factory instantiation

--- src/Pessoas.Integracao.Worker/Infrastructure/Extensions/DependencyInjection.cs
+++ src/Pessoas.Integracao.Worker/Infrastructure/Extensions/DependencyInjection.cs
@@ -4,6 +4,7 @@ using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Configuration;
 using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Channel;
 using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Clients;
 using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Contracts;
+using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Generated.Deltas;
 using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Generated.Output;
 
 namespace Pessoas.Integracao.Worker.Infrastructure.Extensions;
@@ -12,11 +13,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddExternalSoapClientServices(this IServiceCollection services, IConfiguration configuration)
     {
-
-        return services.AddScoped<ISoapChannelProvider<zhr_wsChannel>, SoapChannelProvider<zhr_wsChannel>>()
+        return services
+            .Configure<DataSourceSettings>(configuration.GetSection(DataSourceSettings.SectionName))
+            .AddSoapChannelFactorySingleton<zhr_wsChannel>(settings => settings.OutputUrl)
+            .AddSoapChannelFactorySingleton<ZHR_WS_DELTASChannel>(settings => settings.DeltasUrl)
+            .AddScoped<ISoapChannelProvider<zhr_wsChannel>, SoapChannelProvider<zhr_wsChannel>>()
+            .AddScoped<ISoapChannelProvider<ZHR_WS_DELTASChannel>, SoapChannelProvider<ZHR_WS_DELTASChannel>>()
             .AddScoped<IExternalPersonnelNumberClient, ExternalPersonnelNumberClient>()
             .AddScoped<IPessoasDataProvider, SigdnRhPessoasProvider>()
-            .AddScoped<IPessoasImportKeyProvider, SigdnRhPessoasProvider>()
-            .Configure<DataSourceSettings>(configuration.GetSection(DataSourceSettings.SectionName));
+            .AddScoped<IPessoasImportKeyProvider, SigdnRhPessoasProvider>();
     }
 }
