bc68d11aa328709b935b51456ea4347c782a39a6
Raimundo <pacheco.raimundo@marinha.pt>
2026-03-25 19:56:04 +0000

    fix: remove not needed contrat

--- src/Pessoas.Integracao.Core/Application/Contracts/IPessoasDeltasKeyProvider.cs
+++
@@ -1,8 +0,0 @@
-using Pessoas.Integracao.Core.Application.Models;
-
-namespace Pessoas.Integracao.Core.Application.Contracts;
-
-public interface IPessoasDeltasKeyProvider
-{
-    Task<IReadOnlyList<PessoaDeltasKey>> GetPessoasDeltasKeysAsync(TimePeriod timePeriod, CancellationToken ct);
-}
