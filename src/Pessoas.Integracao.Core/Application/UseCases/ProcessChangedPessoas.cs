b7c982f9bd677a3adef288454cdd3e30cec62da3
Raimundo <pacheco.raimundo@marinha.pt>
2026-03-25 20:35:10 +0000

    feat: add ProcessChangedPessoas use case for handling changed pessoas

---
+++ src/Pessoas.Integracao.Core/Application/UseCases/ProcessChangedPessoas.cs
@@ -0,0 +1,37 @@
+using Pessoas.Integracao.Core.Application.Abstractions;
+using Pessoas.Integracao.Core.Application.Contracts;
+using Pessoas.Integracao.Core.Application.Models;
+using Pessoas.Integracao.Core.Domain.Entities;
+
+namespace Pessoas.Integracao.Core.Application.UseCases;
+
+public class ProcessChangedPessoas(
+    IPessoaRepository pessoaRepository,
+    IPessoasDataProvider pessoasDataProvider,
+    IPessoasChangedImportKeyProvider pessoasChangedImportKeyProvider,
+    IPessoaChangeDetector pessoaChangeDetetor,
+    IUnitOfWork unitOfWork)
+{
+    private readonly IPessoaRepository _pessoaRepository = pessoaRepository;
+    private readonly IPessoasDataProvider _pessoasDataProvider = pessoasDataProvider;
+    private readonly IPessoasChangedImportKeyProvider _pessoasChangedImportKeyProvider = pessoasChangedImportKeyProvider;
+    private readonly IPessoaChangeDetector _pessoasDeltaDetector = pessoaChangeDetetor;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    public async Task ExecuteAsync(TimePeriod timePeriod, CancellationToken ct)
+    {
+        var changedImporKeys = await _pessoasChangedImportKeyProvider.GetChangedImportKeysAsync(timePeriod, ct);
+
+        var pessoasChanged = await _pessoasDataProvider.GetPessoasByImportKeysAsync(changedImporKeys, ct);
+
+        var equivalentPessoasInRepo = await _pessoaRepository.GetPessoasByNiiAsync(pessoasChanged.Select(p => p.NII).ToList(), ct);
+
+        var pessoasToUpsert = pessoasChanged
+            .Where(changed => _pessoasDeltaDetector.IsPessoaChanged(
+                changed,
+                equivalentPessoasInRepo.FirstOrDefault(existing => existing.NII == changed.NII) ?? new Pessoa { NII = changed.NII }))
+            .ToList();
+
+        await _pessoaRepository.UpsertAllAsync(pessoasToUpsert, ct);
+        await _unitOfWork.CommitAsync(ct);
+    }
+}
