75f0f64bdb018ec79503b4291f788c388d8712ce
1TEN EN-AEL Santos Carapau <santos.carapau@marinha.pt>
2026-04-28 15:30:55 +0000

    chore: changed variable name for consistency
    Signed-off-by: 1TEN EN-AEL Santos Carapau <santos.carapau@marinha.pt>

--- src/Pessoas.Integracao.Core/Application/UseCases/ProcessChangedPessoas.cs
+++ src/Pessoas.Integracao.Core/Application/UseCases/ProcessChangedPessoas.cs
@@ -15,18 +15,18 @@ public class ProcessChangedPessoas(
     private readonly IPessoaRepository _pessoaRepository = pessoaRepository;
     private readonly IPessoasDataProvider _pessoasDataProvider = pessoasDataProvider;
     private readonly IPessoasChangedImportKeyProvider _pessoasChangedImportKeyProvider = pessoasChangedImportKeyProvider;
-    private readonly IPessoaChangeDetector _pessoasDeltaDetector = pessoaChangeDetetor;
+    private readonly IPessoaChangeDetector _pessoasChangeDetector = pessoaChangeDetetor;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     public async Task ExecuteAsync(TimePeriod timePeriod, CancellationToken ct)
     {
-        var changedImporKeys = await _pessoasChangedImportKeyProvider.GetChangedImportKeysAsync(timePeriod, ct);
+        var changedImportKeys = await _pessoasChangedImportKeyProvider.GetChangedImportKeysAsync(timePeriod, ct);
 
-        var pessoasChanged = await _pessoasDataProvider.GetPessoasByImportKeysAsync(changedImporKeys, ct);
+        var pessoasChanged = await _pessoasDataProvider.GetPessoasByImportKeysAsync(changedImportKeys, ct);
 
         var equivalentPessoasInRepo = await _pessoaRepository.GetPessoasByNiiAsync(pessoasChanged.Select(p => p.NII).ToList(), ct);
 
         var pessoasToUpsert = pessoasChanged
-            .Where(changed => _pessoasDeltaDetector.IsPessoaChanged(
+            .Where(changed => _pessoasChangeDetector.IsPessoaChanged(
                 changed,
                 equivalentPessoasInRepo.FirstOrDefault(existing => existing.NII == changed.NII) ?? new Pessoa { NII = changed.NII }))
             .ToList();
