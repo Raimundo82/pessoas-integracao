ba292840c459330f9d71ebf5ffae6dcb053d7401
m11003725 <alves.devesa@marinha.pt>
2026-05-08 16:05:22 +0000

    feat: add indicacoesTemp client to fetch colocacoes data

---
+++ src/Pessoas.Integracao.Worker/Infrastructure/Sigdn.Rh/Soap/Contracts/IIndicacoesTempClient.cs
@@ -0,0 +1,9 @@
+﻿using Pessoas.Integracao.Core.Application.Models;
+using Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Generated.Output;
+
+namespace Pessoas.Integracao.Worker.Infrastructure.Sigdn.Rh.Soap.Contracts;
+
+public interface IIndicacoesTempClient
+{
+    Task<Dictionary<PessoaImportKey, ZhrSTemposervOutput?>> GetIndicacoesTempAsync(IReadOnlyList<PessoaImportKey> importKey, CancellationToken cancellationToken);
+}
