2928c8797babceb6cb6d6587775283c00a9faa7f
m22600 <pacheco.raimundo@marinha.pt>
2026-05-12 22:46:16 +0000

    test: enhance unit tests for Pessoa.UpdateFrom method to cover additional scenarios

--- tests/Pessoas.Integracao.Tests/Unit/Domain/Entities/PessoaUpdateFromUnitTests.cs
+++ tests/Pessoas.Integracao.Tests/Unit/Domain/Entities/PessoaUpdateFromUnitTests.cs
@@ -1,86 +1,107 @@
-using System.Collections;
-
 using FluentAssertions;
 
 using Pessoas.Integracao.Core.Domain.Entities;
 using Pessoas.Integracao.Core.Domain.Enums;
-
+using Pessoas.Integracao.Core.Domain.ValueObjects;
 
 namespace Pessoas.Integracao.Tests.Unit.Domain.Entities;
 
 public sealed class PessoaUpdateFromUnitTests
 {
-
-    [Theory]
-    [ClassData(typeof(PessoaTestData))]
-    public void ShouldUpdateAllProperties_WhenSourceHasValidData(Pessoa source, Pessoa existing)
+    [Fact]
+    public void ShouldUpdateExternalId_WhenSourceHasExternalId()
     {
-        // Act
+        var existing = new Pessoa { NII = "123", ExternalId = "old-ext" };
+        var source = new Pessoa { NII = "123", ExternalId = "new-ext" };
+
         existing.UpdateFrom(source);
 
-        // Assert
-        existing.NII.Should().Be(source.NII);
-        existing.ExternalId.Should().Be(source.ExternalId);
+        existing.ExternalId.Should().Be("new-ext");
+    }
+
+    [Fact]
+    public void ShouldSetExternalIdToNull_WhenSourceExternalIdIsNull()
+    {
+        var existing = new Pessoa { NII = "123", ExternalId = "old-ext" };
+        var source = new Pessoa { NII = "123", ExternalId = null };
+
+        existing.UpdateFrom(source);
+
+        existing.ExternalId.Should().BeNull();
+    }
+
+    [Fact]
+    public void ShouldUpdateDadosPessoais_WhenSourceHasDadosPessoais()
+    {
+        var existing = new Pessoa { NII = "123", DadosPessoais = new DadosPessoais { NomeCompleto = "Old Name" } };
+        var source = new Pessoa
+        {
+            NII = "123",
+            DadosPessoais = new DadosPessoais
+            {
+                NomeCompleto = "New Name",
+                Sobrenome = "New Sobrenome",
+                Apelidos = "New Apelidos",
+                DataNascimento = new DateOnly(1990, 5, 12)
+            }
+        };
+
+        existing.UpdateFrom(source);
+
         existing.DadosPessoais.Should().BeEquivalentTo(source.DadosPessoais);
+    }
+
+    [Fact]
+    public void ShouldUpdateDadosBiometricos_WhenSourceHasDadosBiometricos()
+    {
+        var existing = new Pessoa { NII = "123", DadosBiometricos = new DadosBiometricos { AlturaEmCm = 170 } };
+        var source = new Pessoa
+        {
+            NII = "123",
+            DadosBiometricos = new DadosBiometricos
+            {
+                AlturaEmCm = 180,
+                CorDosOlhos = "castanhos",
+                TipoDeSangue = new TipoDeSangue { GrupoSanguineo = GrupoSanguineo.O, Rhesus = Rhesus.POSITIVO }
+            }
+        };
+
+        existing.UpdateFrom(source);
+
         existing.DadosBiometricos.Should().BeEquivalentTo(source.DadosBiometricos);
     }
 
-    public class PessoaTestData : IEnumerable<object[]>
+    [Fact]
+    public void ShouldNotChangeNII_AfterUpdate()
     {
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            yield return new Pessoa[] {
-                new() { NII = "123", ExternalId = "Old" },
-                new() { NII = "123" },
-            };
-            yield return new Pessoa[] {
-                new() { NII = "123", ExternalId = "Old" },
-                new() { NII = "123", ExternalId = "New" },
-            };
-            yield return new Pessoa[] {
-                new() { NII = "123", ExternalId = "Old" },
-                new() { NII = "123", ExternalId = "New", DadosPessoais = new(), DadosBiometricos = new() },
-            };
-            yield return new Pessoa[] {
-                new() {
-                    NII = "123",
-                    ExternalId = "Old",
-                    DadosPessoais = new()
-                    {
-                        NomeCompleto = "Nome Completo",
-                        Apelidos = "Completo",
-                        Sobrenome = "Comepleto",
-                        DataNascimento = new DateOnly(2000, 10, 19),
-                    },
-                    DadosBiometricos = new()
-                    {
-                        AlturaEmCm = 180,
-                        CorDosOlhos = "castanhos"
-                    }
-                },
-                new() {
-                    NII = "123",
-                    ExternalId = "New",
-                    DadosPessoais = new()
-                    {
-                        NomeCompleto = "Nome Completo Novo",
-                        Apelidos = "Completo Novo",
-                        Sobrenome = "Novo",
-                        DataNascimento = new DateOnly(199, 10, 19),
-                    },
-                    DadosBiometricos = new()
-                    {
-                        AlturaEmCm = 178,
-                        CorDosOlhos = "azuis",
-                        TipoDeSangue = new() {
-                            GrupoSanguineo = GrupoSanguineo.O,
-                            Rhesus = Rhesus.POSITIVO}
-                    }
-                },
-            };
-        }
+        var existing = new Pessoa { NII = "123" };
+        var source = new Pessoa { NII = "999" };
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        existing.UpdateFrom(source);
+
+        existing.NII.Should().Be("123");
     }
 
+    [Fact]
+    public void ShouldNotChangeId_AfterUpdate()
+    {
+        var existing = new Pessoa { NII = "123", Id = 42 };
+        var source = new Pessoa { NII = "999", Id = 99 };
+
+        existing.UpdateFrom(source);
+
+        existing.Id.Should().Be(42);
+    }
+
+    [Fact]
+    public void ShouldNotChangeColocacoes_AfterUpdate()
+    {
+        var existing = new Pessoa { NII = "123" };
+        var originalColocacoes = existing.Colocacoes;
+        var source = new Pessoa { NII = "999" };
+
+        existing.UpdateFrom(source);
+
+        existing.Colocacoes.Should().BeSameAs(originalColocacoes);
+    }
 }
