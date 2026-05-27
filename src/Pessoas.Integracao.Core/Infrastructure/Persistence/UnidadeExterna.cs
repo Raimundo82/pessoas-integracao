b9e8c08815f87a229efbcada3681e1ee09209531
Raimundo <pacheco.raimundo@marinha.pt>
2026-05-27 22:57:02 +0000

    chore: remove UnidadeExterna model from persistence layer

--- src/Pessoas.Integracao.Core/Infrastructure/Persistence/UnidadeExterna.cs
+++
@@ -1,8 +0,0 @@
-namespace Pessoas.Integracao.Core.Infrastructure.Persistence;
-
-public class UnidadeExterna
-{
-    public int Id { get; set; }
-    public required string ExternalId { get; set; }
-    public string? Nome { get; set; }
-}
