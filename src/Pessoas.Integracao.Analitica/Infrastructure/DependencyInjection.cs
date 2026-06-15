fb6496cf3833dbcc3fc79a8cd7766e99cb332523
ESP SIST TECN INF Alves Devesa <alves.devesa@marinha.pt>
2026-06-15 16:16:05 +0000

    refactor: replaces manual scoped registrations of specific entities with a generic registration for IAnaliticaRepository<T>

--- src/Pessoas.Integracao.Analitica/Infrastructure/DependencyInjection.cs
+++ src/Pessoas.Integracao.Analitica/Infrastructure/DependencyInjection.cs
@@ -1,22 +1,14 @@
-
-using Pessoas.Integracao.Analitica.Infrastructure.Data;
+using Pessoas.Integracao.Analitica.Application.Contracts;
 using Pessoas.Integracao.Analitica.Infrastructure.Repositories;
 
-using Pessoas.Integracao.Analitica.Models;
 namespace Pessoas.Integracao.Analitica.Infrastructure;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
-        services.AddScoped(sp =>
-            new AnaliticaRepository<ZhrWsAptidaoAptidao>(
-                sp.GetRequiredService<AnaliticaDbContext>()));
-
-        services.AddScoped(sp =>
-            new AnaliticaRepository<ZhrWsAtribOrgAtribOrg>(
-                sp.GetRequiredService<AnaliticaDbContext>()));
-
+        services.AddScoped(typeof(IAnaliticaRepository<>), typeof(AnaliticaRepository<>));
         return services;
     }
 }
+
