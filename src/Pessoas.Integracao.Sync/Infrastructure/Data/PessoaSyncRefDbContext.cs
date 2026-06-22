43335db801a9b3ec75c1a80be8d03410fd38232b
Raimundo <pacheco.raimundo@marinha.pt>
2026-06-22 15:13:19 +0100

    refactor: remove redundant Ni (PK) non nullable constraint

--- src/Pessoas.Integracao.Sync/Infrastructure/Data/PessoaSyncRefDbContext.cs
+++ src/Pessoas.Integracao.Sync/Infrastructure/Data/PessoaSyncRefDbContext.cs
@@ -14,10 +14,6 @@ public class PessoaSyncRefDbContext(DbContextOptions<PessoaSyncRefDbContext> opt
 
         modelBuilder.Entity<PessoaSyncRef>(entity =>
         {
-            entity.Property(e => e.Ni)
-                .IsRequired()
-                .HasMaxLength(255);
-
             entity.Property(e => e.ExternalId)
                 .IsRequired()
                 .HasMaxLength(255);
