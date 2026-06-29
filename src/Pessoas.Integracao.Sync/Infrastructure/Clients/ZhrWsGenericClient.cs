74ee21b076e68bcfe9c1922ee6f9518d8877d7d5
Raimundo <pacheco.raimundo@marinha.pt>
2026-06-29 18:58:07 +0100

    feat(sync): implement ZhrWsGenericClient for generic SOAP communication

---
+++ src/Pessoas.Integracao.Sync/Infrastructure/Clients/ZhrWsGenericClient.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Options;
+
+using Pessoas.Integracao.Sync.Domain.Entities;
+using Pessoas.Integracao.Sync.Infrastructure.Configuration;
+using Pessoas.Integracao.Sync.Infrastructure.Factories;
+using Pessoas.Integracao.Sync.Infrastructure.Models.Dados;
+
+namespace Pessoas.Integracao.Sync.Infrastructure.Clients;
+
+public class ZhrWsGenericClient(
+    IZhrWsGenericClientFactory<ZHR_WSClient, ZHR_WS> clientFactory,
+    IOptions<ZhrWsSettings> settings
+) : IZhrWsGenericClient
+{
+    private readonly IZhrWsGenericClientFactory<ZHR_WSClient, ZHR_WS> _clientFactory = clientFactory;
+    private readonly ZhrWsSettings _settings = settings.Value;
+
+    public async Task<TResponse> CallAsync<TResponse>(
+        Func<ZHR_WSClient, ZhrWsInputStruct[], Task<TResponse>> zhrWsOperation,
+        IReadOnlyCollection<PessoaSyncRef> pessoaSyncRefs,
+        CancellationToken ct = default
+    ) where TResponse : IZhrWsBaseResponse
+    {
+        var client = _clientFactory.CreateClient();
+        var inputs = pessoaSyncRefs.Select(pessoaRef =>
+            new ZhrWsInputStruct
+            {
+                Ni = pessoaRef.Ni,
+                Numsap = pessoaRef.ExternalId,
+                Empresa = _settings.Empresa,
+
+            }).ToArray();
+
+        return await zhrWsOperation(client, inputs);
+    }
+}
