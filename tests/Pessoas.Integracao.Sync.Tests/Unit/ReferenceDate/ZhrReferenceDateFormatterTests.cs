4ab977c3436fe2b6c51e05fe065ba4b357967285
Raimundo <pacheco.raimundo@marinha.pt>
2026-06-30 08:54:23 +0000

    feat(sync): add `ZhrReferenceDateFormatter` (#666)
    ## Checklist
    
    - [x] Branch name follows **Conventional Branch**
    - [x] PR titles and Commit messages follow **Conventional Commits**
    
    Closes #665
    
    Reviewed-on: https://devops-01.marinha.pt/marinha-si/pessoas-integracao/pulls/666
    Co-authored-by: Raimundo <pacheco.raimundo@marinha.pt>
    Co-committed-by: Raimundo <pacheco.raimundo@marinha.pt>

---
+++ tests/Pessoas.Integracao.Sync.Tests/Unit/ReferenceDate/ZhrReferenceDateFormatterTests.cs
@@ -0,0 +1,70 @@
+using FluentAssertions;
+
+using Microsoft.Extensions.Time.Testing;
+
+using Pessoas.Integracao.Sync.Infrastructure.Services.ReferenceDate;
+
+namespace Pessoas.Integracao.Sync.Tests.Unit.ReferenceDate;
+
+public class ZhrReferenceDateFormatterTests
+{
+    [Fact]
+    public void ShouldReturnFormattedDate_WhenDateIsPast()
+    {
+        // Arrange
+        var timeProviderMock = new FakeTimeProvider(DateTimeOffset.UtcNow);
+        var date = new DateOnly(2023, 1, 1);
+        var uut = new ZhrReferenceDateFormatter(timeProviderMock);
+
+        // Act
+        var result = uut.Format(date);
+
+        // Assert
+        result.Should().Be("2023-01-01");
+
+    }
+    [Fact]
+    public void ShouldReturnFormattedDate_WhenDateIsToday()
+    {
+        // Arrange
+        var timeProviderMock = new FakeTimeProvider(new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        var date = new DateOnly(2023, 1, 1);
+        var uut = new ZhrReferenceDateFormatter(timeProviderMock);
+
+        // Act
+        var result = uut.Format(date);
+
+        // Assert
+        result.Should().Be("2023-01-01");
+    }
+
+    [Fact]
+    public void ShouldReturnTodayDate_WhenDateIsFuture()
+    {
+        // Arrange
+        var timeProviderMock = new FakeTimeProvider(new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        var date = new DateOnly(2024, 1, 1);
+        var uut = new ZhrReferenceDateFormatter(timeProviderMock);
+
+        // Act
+        var result = uut.Format(date);
+
+        // Assert
+        result.Should().Be("2023-01-01");
+    }
+
+    [Fact]
+    public void ShouldReturnIsoFormat_WhenDateIsValid()
+    {
+        // Arrange
+        var timeProviderMock = new FakeTimeProvider(DateTimeOffset.UtcNow);
+        var date = new DateOnly(2023, 12, 25);
+        var uut = new ZhrReferenceDateFormatter(timeProviderMock);
+
+        // Act
+        var result = uut.Format(date);
+
+        // Assert
+        result.Should().MatchRegex(@"^\d{4}-\d{2}-\d{2}$");
+    }
+}
