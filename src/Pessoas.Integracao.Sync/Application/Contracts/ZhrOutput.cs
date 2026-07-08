a57b4342a51e295d451b743f2803a99206159d71
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-08 22:57:51 +0100

    feat: define ZhrOutput contract and IZhrOutputProvider interface

--- src/Pessoas.Integracao.Sync/Application/Contracts/ZhrOutput.cs
+++ src/Pessoas.Integracao.Sync/Application/Contracts/ZhrOutput.cs
@@ -2,7 +2,7 @@ using Pessoas.Integracao.Sync.Application.ZhrModels.Dados;
 
 namespace Pessoas.Integracao.Sync.Application.Contracts;
 
-public class ZhrOutputDto
+public class ZhrOutput
 {
     public required string Ni { get; init; }
     public required string ExternalId { get; init; }
