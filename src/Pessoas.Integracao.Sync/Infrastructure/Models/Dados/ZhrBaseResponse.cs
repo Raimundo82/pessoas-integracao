1cdbc1ad62dded56a2b2cac8c65b6c55b0696d4b
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-08 22:19:52 +0100

    chore: remove not needed cs modules

--- src/Pessoas.Integracao.Sync/Infrastructure/Models/Dados/ZhrBaseResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Pessoas.Integracao.Sync.Infrastructure.Models.Dados;
-
-
-public interface IZhrWsBaseResponse { }
-
-public partial class ZhrWsAptidaoResponse1 : IZhrWsBaseResponse { }
-
-public partial class ZhrWsPersonalDataResponse1 : IZhrWsBaseResponse { }
--- src/Pessoas.Integracao.Sync/Infrastructure/Models/Dados/ZhrSBaseModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Pessoas.Integracao.Sync.Infrastructure.Models.Dados;
-
-public abstract class ZhrSBaseModel
-{
-    public int Id { get; set; }
-    public required string Ni { get; set; }
-
-}
