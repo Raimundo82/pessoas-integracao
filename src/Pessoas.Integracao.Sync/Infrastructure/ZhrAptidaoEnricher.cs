e848af00482762beb644abe4451b215f20dfe439
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-13 16:51:30 +0100

    feat: add Aptidao enricher

---
+++ src/Pessoas.Integracao.Sync/Infrastructure/ZhrAptidaoEnricher.cs
@@ -0,0 +1,29 @@
+namespace Pessoas.Integracao.Sync.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+using Pessoas.Integracao.Sync.Application.Contracts;
+using Pessoas.Integracao.Sync.Application.ZhrModels.Dados;
+using Pessoas.Integracao.Sync.Domain.Entities;
+using Pessoas.Integracao.Sync.Infrastructure.Data;
+
+public class ZhrAptidaoEnricher(ZhrSDbContext dbContext) : IZhrOutputsEnricher
+{
+
+    public async Task EnrichAsync(
+        IReadOnlyList<PessoaSyncRef> pessoaSyncRefs,
+        IReadOnlyList<ZhrOutput> zhrOutputs,
+        CancellationToken ct)
+    {
+        var niis = pessoaSyncRefs.Select(p => p.Ni);
+        var aptidoes = await dbContext
+            .Set<ZhrSAptidao>()
+            .Where(item => niis.Contains(item.Ni))
+            .ToListAsync(cancellationToken: ct);
+
+        foreach (var output in zhrOutputs)
+        {
+            output.Aptidoes.AddRange(aptidoes.Where(p => p.Ni == output.Ni));
+        }
+    }
+}
