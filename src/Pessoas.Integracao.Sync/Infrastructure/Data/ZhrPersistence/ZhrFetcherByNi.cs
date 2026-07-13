27f3bce945c9000e5ad5fd81e5081c25ba4c9ca5
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-13 23:28:33 +0100

    feat(sync): add ZhrFetcherByNi to query ZhrS* models by Ni

---
+++ src/Pessoas.Integracao.Sync/Infrastructure/Data/ZhrPersistence/ZhrFetcherByNi.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+using Pessoas.Integracao.Sync.Application.ZhrModels.Dados;
+using Pessoas.Integracao.Sync.Domain.Entities;
+
+namespace Pessoas.Integracao.Sync.Infrastructure.Data.ZhrPersistence;
+
+public sealed class ZhrFetcherByNi(ZhrSDbContext dbContext)
+{
+
+    public async Task<IEnumerable<T>> ExecuteAsync<T>(
+            IReadOnlyList<PessoaSyncRef> pessoaSyncRefs,
+            CancellationToken ct
+        ) where T : ZhrSBaseModel
+    {
+        var nis = pessoaSyncRefs.Select(p => p.Ni).ToList();
+        return await dbContext.Set<T>()
+            .Where(x => nis.Contains(x.Ni))
+            .ToListAsync(cancellationToken: ct);
+    }
+}
