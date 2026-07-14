27b7040df08736caf735d684b777257bf01f7871
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-14 13:35:17 +0100

    tests: add test for empty input and large input

--- tests/Pessoas.Integracao.Sync.Tests/Integration/Infrastructure/ZhrFetcherByNiDbIntegrationTests.cs
+++ tests/Pessoas.Integracao.Sync.Tests/Integration/Infrastructure/ZhrFetcherByNiDbIntegrationTests.cs
@@ -126,4 +126,45 @@ public sealed class ZhrFetcherByNiDbIntegrationTests(PostgresTestContainerDb db)
         result.Where(i => i.Ni == ni2).Should().HaveCount(1);
     }
 
+    [Fact]
+    public async Task ShouldReturnEmpty_WhenInputIsEmpty()
+    {
+        // Arrange
+        string ni1 = "0001", externalId1 = "30001";
+        string ni2 = "0002", externalId2 = "30002";
+        string ni3 = "0003", externalId3 = "30003";
+
+        await SeedAsync(AptidaoOutput(ni1, externalId1), [AptidaoChild(ni1, "Aptidao1")]);
+        await SeedAsync(AptidaoOutput(ni2, externalId2), [AptidaoChild(ni2, "Aptidao1")]);
+        await SeedAsync(AptidaoOutput(ni3, externalId3), [AptidaoChild(ni3, "Aptidao1")]);
+
+
+        // Act
+        await using var actContext = NewContext();
+        var result = await new ZhrFetcherByNi(actContext).ExecuteAsync<ZhrSAptidao>([], _ct);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task ShouldReturnLargeList_WhenInputIsLarge()
+    {
+        // Arrange
+        var inputs = new List<PessoaSyncRef>();
+        for (int i = 0; i < 1_000; i++)
+        {
+            string ni = $"000{i}", externalId = $"3000{i}";
+            inputs.Add(new PessoaSyncRef { Ni = ni, ExternalId = externalId });
+            await SeedAsync(AptidaoOutput(ni, externalId), [AptidaoChild(ni, "Aptidao1")]);
+        }
+
+        // Act
+        await using var actContext = NewContext();
+        var result = await new ZhrFetcherByNi(actContext).ExecuteAsync<ZhrSAptidao>(inputs, _ct);
+
+        // Assert
+        result.Should().HaveCount(1000);
+    }
+
 }
