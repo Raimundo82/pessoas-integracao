30c2c48f414c6f93597729fd52c06bee93a13ce6
ESP SIST TECN INF Alves Devesa <alves.devesa@marinha.pt>
2026-07-17 13:49:54 +0000

    feat: implement SyncAnaliticaData use case to orchestrate data synchronization

---
+++ src/Pessoas.Integracao.Analitica/Application/UseCases/SyncAnaliticaData.cs
@@ -0,0 +1,15 @@
+﻿using Pessoas.Integracao.Analitica.Infrastructure.SyncHandlers;
+using Pessoas.Integracao.Sync.Application.Contracts;
+
+namespace Pessoas.Integracao.Analitica.Application.UseCases;
+
+public sealed class SyncAnaliticaData(IEnumerable<IZhrCollectionSyncHandler> syncHandlers)
+{
+    public async Task ExecuteAsync(ZhrOutput input, CancellationToken ct)
+    {
+        foreach (var handler in syncHandlers)
+        {
+            await handler.SyncAsync(input, ct);
+        }
+    }
+}
