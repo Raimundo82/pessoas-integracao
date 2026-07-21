df40bd4a74773efe478ff24c38c92766f267ca1a
ESP SIST TECN INF Alves Devesa <alves.devesa@marinha.pt>
2026-07-21 14:32:53 +0000

    refactor: added covariance and contravariance to ensure that the interface can be used with more generic types without breaking compatibility.

--- src/Pessoas.Integracao.Analitica/Infrastructure/Mappers/IEntityMapper.cs
+++ src/Pessoas.Integracao.Analitica/Infrastructure/Mappers/IEntityMapper.cs
@@ -1,6 +1,6 @@
 namespace Pessoas.Integracao.Analitica.Infrastructure.Mappers;
 
-public interface IEntityMapper<TSource, TTarget>
+public interface IEntityMapper<in TSource, out TTarget>
 {
     TTarget Map(TSource source, string numsap);
 }
