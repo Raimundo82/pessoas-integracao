26096714de5e018950c469409575d852dbacb261
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-23 00:07:00 +0100

    refactor(analitica): change UpdateAt to DateTimeOffset

--- src/Pessoas.Integracao.Analitica/Models/ZhrWsBaseModel.cs
+++ src/Pessoas.Integracao.Analitica/Models/ZhrWsBaseModel.cs
@@ -4,6 +4,6 @@ public abstract class ZhrWsBaseModel
 {
     public int Id { get; set; }
     public required string Ni { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
 
 }
