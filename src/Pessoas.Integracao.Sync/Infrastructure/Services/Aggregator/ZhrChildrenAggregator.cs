460ae00c5e1908a85bbb10c2bf5371d0574346eb
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-23 20:30:42 +0100

    refactor(sync): handle null outputs in Aggregate method of ZhrChildrenAggregator

--- src/Pessoas.Integracao.Sync/Infrastructure/Services/Aggregator/ZhrChildrenAggregator.cs
+++ src/Pessoas.Integracao.Sync/Infrastructure/Services/Aggregator/ZhrChildrenAggregator.cs
@@ -6,7 +6,7 @@ public sealed class ZhrChildrenAggregator : IZhrChildrenAggregator
 {
     public IReadOnlyList<ZhrSBaseModel[]> Aggregate(IReadOnlyList<ZhrSBaseModelOutput> outputs)
     {
-        return [.. outputs
+        return [.. (outputs ?? [])
             .SelectMany(x => x.GetChildrenFlattened())
             .GroupBy(x => x.GetType())
             .Select(g => g.ToArray())];
