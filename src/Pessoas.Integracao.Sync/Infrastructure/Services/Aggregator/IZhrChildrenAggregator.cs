1f3ef02c1137c123525f5a6ed58fbb310df3d2b1
João Raimundo <pacheco.raimundo@marinha.pt>
2026-07-27 19:51:04 +0000

    feat(sync): add `ZhrChildrenAggregator` service (#732)
    ## Checklist
    
    - [X] Branch name follows **Conventional Branch**
    - [X] PR titles and Commit messages follow **Conventional Commits**Reviewed-on: https://devops-01.marinha.pt/marinha-si/pessoas-integracao/pulls/732

---
+++ src/Pessoas.Integracao.Sync/Infrastructure/Services/Aggregator/IZhrChildrenAggregator.cs
@@ -0,0 +1,8 @@
+using Pessoas.Integracao.Sync.Application.ZhrModels.Dados;
+
+namespace Pessoas.Integracao.Sync.Infrastructure.Services.Aggregator;
+
+public interface IZhrChildrenAggregator
+{
+    IReadOnlyList<ZhrSBaseModel[]> Aggregate(IReadOnlyList<IOutputModel> outputs);
+}
