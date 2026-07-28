ce2428d1190b5d4e98c5902adfd798bdb5f5fc23
Raimundo <pacheco.raimundo@marinha.pt>
2026-07-28 22:51:17 +0100

    refactor: rename response classes and interfaces for consistency and to align with zhr responses

---
+++ src/Pessoas.Integracao.Sync/Application/ZhrModels/Dados/ZhrBaseResponse1.cs
@@ -0,0 +1,7 @@
+namespace Pessoas.Integracao.Sync.Application.ZhrModels.Dados;
+
+public interface IZhrWsBaseResponse1 { }
+
+public partial class ZhrWsAptidaoResponse1 : IZhrWsBaseResponse1 { }
+public partial class ZhrWsAtribOrgResponse1 : IZhrWsBaseResponse1 { }
+public partial class ZhrWsPersonalDataResponse1 : IZhrWsBaseResponse1 { }
